0373a592aed10501976f6352149a3e196cc2d431
MichelaRicci <michela.ricci.1991@gmail.com>
2022-12-18 14:29:34 +0100

    milestone 3 (numeri 1-100 visualizzati)

--- js/script.js
+++ js/script.js
@@ -4,13 +4,28 @@ console.log('JS OK');
 // MILESTONE 1
 // Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
 // MILESTONE 2
-// Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
+// Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. 
+// Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
 // MILESTONE 3
 // In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
 // #MILESTONE 4
 // Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
 
 
+// FUNCTIONS ----------------------------------------------------------
+// Creazione celle 
+const createCell = (content) => {
+
+    const cell = document.createElement('div');
+
+    cell.classList.add('cell');
+
+    cell.append(content);
+
+    return cell;
+
+    
+}
 // Prendo elementi dalla pagina 
 
 const grid = document.getElementById('grid');
@@ -31,21 +46,22 @@ play.addEventListener('click', function(){
 
 
 
-//Randomizzo 100 celle
-for(let i = 0; i < totalCells; i++) {
-    
-    // Creazione cella
-    const cell = document.createElement('div');
-    cell.classList.add('cell');
+    //Randomizzo 100 celle
+    for(let i = 1; i < totalCells; i++) {
+        
+        // Creazione cella
+        const cell = createCell(i);
 
 
-    // Aggiungo eventListener 
-    cell.addEventListener('click', function() {
-        cell.classList.add('clicked');
-    });
+        // Aggiungo eventListener 
+        cell.addEventListener('click', () => {
+            cell.classList.add('clicked');
 
-    grid.appendChild(cell);
-}
+        });
+
+        grid.appendChild(cell);
+    }
+
 
 })
 
